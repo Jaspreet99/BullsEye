51ec2387c78683721acdae9183ca313dcd7fc1cc
Jaspreet Singh <Jaspreet18csu094@ncuindia.edu>
2019-01-22 22:08:31 +0530

    Initial Commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Jaspreet on 16/12/18.
+//  Copyright © 2018 Jaspreet. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
